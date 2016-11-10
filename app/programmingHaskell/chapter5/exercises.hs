f1d6b91806ab93b5a4141db6b16f2d617691e2b7
Martin Rist <mlr-github@martinrist.com>
2016-11-10 09:23:29 +0000

    Complete chapter 5 Type-Kwon-Do exercises

--- app/programmingHaskell/chapter5/exercises.hs
+++ app/programmingHaskell/chapter5/exercises.hs
@@ -24,3 +24,49 @@ a f x = x
 
 a' :: (a -> b) -> a -> b
 a' f x = f x
+
+------------------
+-- Type-Kwon-Do --
+------------------
+
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h = g . f
+
+
+
+data A
+data B
+data C
+
+q :: A -> B
+q = undefined
+
+w :: B -> C
+w = undefined
+
+e :: A -> C
+e = w . q
+
+
+data X
+data Y
+data Z
+
+xz :: X -> Z
+xz = undefined
+
+yz :: Y -> Z
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z)
+xform (x, y) = (xz x, yz y)
+
+
+munge :: (x -> y) -> (y -> (w, z)) -> x -> w
+munge xy ywz x = fst $ ywz $ xy $ x
