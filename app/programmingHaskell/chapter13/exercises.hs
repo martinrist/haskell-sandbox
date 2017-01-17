b6b682825861d60e90e26d97f07b97867d5b2b0e
Martin Rist <mlr-github@martinrist.com>
2017-01-17 13:05:21 +0000

    Complete chapter 13 exercises

---
+++ app/programmingHaskell/chapter13/exercises.hs
@@ -0,0 +1,67 @@
+-----------------------
+-- Chapter Exercises --
+-----------------------
+import Control.Monad
+import System.Exit (exitSuccess)
+import Data.Char (toLower)
+
+-- Exercises 2 & 3
+
+palindrome :: IO ()
+palindrome = forever $ do
+    line1 <- getLine
+    case isPalindrome line1 of
+        True  -> putStrLn "It's a palindrome!"
+        False -> do
+            putStrLn "Nope!"
+            exitSuccess
+
+invalidChars :: [Char]
+invalidChars = " '"
+
+filterChars :: String -> String
+filterChars = filter (`notElem` invalidChars)
+
+prepare :: String -> String
+prepare = map toLower . filterChars
+
+isPalindrome :: String -> Bool
+isPalindrome s = let s' = map toLower (filterChars s) in
+                     s' == reverse s'
+
+
+-- Exercise 4
+
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age deriving Show
+
+data PersonInvalid = NameEmpty
+                   | AgeTooLow
+                   | PersonInvalidUnknown String
+                   deriving (Eq, Show)
+
+mkPerson :: Name -> Age -> Either PersonInvalid Person
+mkPerson name age
+    | name /= "" && age > 0 = Right $ Person name age
+    | name == ""            = Left NameEmpty
+    | age <= 0              = Left AgeTooLow
+    | otherwise             = Left $ PersonInvalidUnknown $
+                                "Name was: " ++ show name ++
+                                " Age was: " ++ show age
+
+gimmePerson :: IO ()
+gimmePerson = do
+    putStr "Enter name: "
+    name <- getLine
+    putStr "Enter age: "
+    age <- getLine
+    let person = mkPerson name (read age) in
+        outputPerson person
+
+
+outputPerson :: Either PersonInvalid Person -> IO ()
+outputPerson (Right p) = putStrLn $ "Yay! Successfully got a person: " ++ show p
+outputPerson (Left i)  = putStrLn $ "Error: " ++ show i
+
