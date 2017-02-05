55c91cb27b00f6031335be78ca5501aa815eb264
Martin Rist <mlr-github@martinrist.com>
2017-02-05 18:56:28 +0000

    Complete exercise 1

--- app/programmingHaskell/chapter16/exercises.hs
+++ app/programmingHaskell/chapter16/exercises.hs
@@ -1,6 +1,8 @@
 ------------------------------
 -- Exercises: Heavy Lifting --
 ------------------------------
+import Test.QuickCheck
+import Test.QuickCheck.Function
 
 -- Exercise 1
 a :: [Int]
@@ -23,3 +25,38 @@ e :: IO Integer
 e = let ioi = readIO "1" :: IO Integer
         changed = fmap (read . ("123" ++) . show) ioi
         in fmap (*3) changed
+
+
+----------------------------------
+-- Exercises: Instances of Func --
+----------------------------------
+
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f = fmap id f == f
+
+functorCompose :: (Eq (f c), Functor f) => (a -> b) -> (b -> c) -> f a -> Bool
+functorCompose f g x = (fmap g (fmap f x)) == (fmap (g  . f) x)
+
+functorCompose' :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x)
+
+-- Exercise 1 - Identity
+newtype Identity a = Identity a
+    deriving (Eq, Show)
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = do
+        a <- arbitrary
+        return (Identity a)
+
+type IntToInt = Fun Int Int
+
+exercise1 :: IO ()
+exercise1 = do
+    quickCheck $ \x -> functorIdentity (x :: Identity Int)
+    quickCheck (functorCompose' :: Identity Int -> IntToInt -> IntToInt -> Bool)
+
+
