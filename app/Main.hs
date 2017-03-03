fb9c3feb1827a71c5a0d83c07b0892c5172cbe9f
Martin Rist <mlr-github@martinrist.com>
2017-03-03 14:21:29 +0000

    Add example of using optparsers-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,57 @@
 module Main where
 
 import Lib
+import Options.Applicative.Common
+import Options.Applicative.Builder
+import Options.Applicative.Extra
+import Control.Monad
+import Control.Applicative
+
+data Sample = Sample
+    { hello :: String
+    , repeat :: Int
+    , quiet :: Bool }
+
+helloOption :: Parser String
+helloOption = strOption
+                ( long "hello"
+                <> metavar "TARGET"
+                <> help "Target for the greeting" )
+
+quietOption :: Parser Bool
+quietOption = switch
+               ( long "quiet"
+               <> help "Whether to be quiet" )
+
+repeatOption :: Parser Int
+repeatOption = option auto
+                ( long "repeat"
+                <> help "Repeats for greeting"
+                <> showDefault
+                <> value 1
+                <> metavar "INT" )
+
+sample :: Parser Sample
+sample = Sample <$> helloOption <*> repeatOption <*> quietOption
+-- This also works:
+-- sample = liftA3 Sample helloOption repeatOption quietOption
+
+opts :: ParserInfo Sample
+opts = info (helper <*> sample)
+            ( fullDesc
+            <> progDesc "Print a greeting for TARGET"
+            <> header "hello - a test for optparse-applicative" )
+
+greet :: Sample -> IO ()
+greet (Sample h n False) = replicateM_ n . putStrLn $ "Hello, " ++ h
+greet _ = return ()
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= greet
+
+-- Alternative implementation using 'do'
+--main :: IO ()
+--main = do
+--    o <- execParser opts
+--    greet o
+
