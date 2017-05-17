ef4d6740466bfaf2306f5c3ca36885f1f7685dee
Martin Rist <mlr-github@martinrist.com>
2017-05-17 15:49:25 +0100

    Complete Traversable instance for 'S'

--- app/programmingHaskell/chapter21/chapterExercises.hs
+++ app/programmingHaskell/chapter21/chapterExercises.hs
@@ -1,4 +1,6 @@
-import           GHC.Generics
+{-# LANGUAGE FlexibleContexts #-}
+
+import           Data.Monoid              ((<>))
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
@@ -7,15 +9,12 @@ import           Test.QuickCheck.Function
 ------------------------------------
 -- Chapter 21 - Chapter Exercises --
 ------------------------------------
-
 main :: IO ()
 main = undefined
 
 -- Traversable instances
 ------------------------
-
 -- Identity
-
 newtype Identity a =
     Identity a
     deriving (Eq, Show)
@@ -32,24 +31,23 @@ instance Traversable Identity where
 identityTrigger :: Identity (Int, Int, [Int])
 identityTrigger = undefined
 
-instance Arbitrary a => Arbitrary (Identity a) where
+instance Arbitrary a =>
+         Arbitrary (Identity a) where
     arbitrary = do
         a <- arbitrary
         return $ Identity a
 
-instance Eq a => EqProp (Identity a) where
+instance Eq a =>
+         EqProp (Identity a) where
     (=-=) = eq
 
 testIdentityTraversable :: IO ()
 testIdentityTraversable = quickBatch $ traversable identityTrigger
 
-
-
 -- Constant
-
-newtype Constant a b =
-    Constant { getConstant :: a}
-    deriving (Eq, Show)
+newtype Constant a b = Constant
+    { getConstant :: a
+    } deriving (Eq, Show)
 
 instance Functor (Constant a) where
     fmap f = Constant . getConstant
@@ -60,12 +58,14 @@ instance Foldable (Constant a) where
 instance Traversable (Constant a) where
     traverse f x = Constant <$> pure (getConstant x)
 
-instance Arbitrary a => Arbitrary (Constant a b) where
+instance Arbitrary a =>
+         Arbitrary (Constant a b) where
     arbitrary = do
         a <- arbitrary
         return $ Constant a
 
-instance Eq a => EqProp (Constant a b) where
+instance Eq a =>
+         EqProp (Constant a b) where
     (=-=) = eq
 
 constantTrigger :: Constant (Int, Int, [Int]) (Int, Int, [Int])
@@ -74,11 +74,9 @@ constantTrigger = undefined
 testConstantTraversable :: IO ()
 testConstantTraversable = quickBatch $ traversable constantTrigger
 
-
 -- Maybe
-
-data Optional a =
-      Nada
+data Optional a
+    = Nada
     | Yep a
     deriving (Eq, Show)
 
@@ -94,12 +92,12 @@ instance Traversable Optional where
     traverse _ Nada    = pure Nada
     traverse f (Yep a) = Yep <$> f a
 
-instance Arbitrary a => Arbitrary (Optional a) where
-    arbitrary =
-        frequency [(1, return Nada),
-                   (3, fmap Yep arbitrary)]
+instance Arbitrary a =>
+         Arbitrary (Optional a) where
+    arbitrary = frequency [(1, return Nada), (3, fmap Yep arbitrary)]
 
-instance Eq a => EqProp (Optional a) where
+instance Eq a =>
+         EqProp (Optional a) where
     (=-=) = eq
 
 optionalTrigger :: Optional (Int, Int, [Int])
@@ -108,11 +106,11 @@ optionalTrigger = undefined
 testOptionalTraversable :: IO ()
 testOptionalTraversable = quickBatch $ traversable optionalTrigger
 
-
 -- List
-data List a =
-      Nil
-    | Cons a (List a)
+data List a
+    = Nil
+    | Cons a
+           (List a)
     deriving (Eq, Show)
 
 instance Functor List where
@@ -130,10 +128,12 @@ instance Traversable List where
 toList :: [a] -> List a
 toList = foldr Cons Nil
 
-instance Arbitrary a => Arbitrary (List a) where
+instance Arbitrary a =>
+         Arbitrary (List a) where
     arbitrary = toList <$> arbitrary
 
-instance Eq a => EqProp (List a) where
+instance Eq a =>
+         EqProp (List a) where
     Nil =-= ys = ys `eq` Nil
     xs =-= Nil = xs `eq` Nil
     Cons x xs =-= Cons y ys = x `eq` y .&. xs `eq` ys
@@ -144,11 +144,11 @@ listTrigger = undefined
 testListTraversable :: IO ()
 testListTraversable = quickBatch $ traversable listTrigger
 
-
 -- Three
-
 data Three a b c =
-    Three a b c
+    Three a
+          b
+          c
     deriving (Eq, Show)
 
 instance Functor (Three a b) where
@@ -160,14 +160,16 @@ instance Foldable (Three a b) where
 instance Traversable (Three a b) where
     traverse f (Three a b c) = Three a b <$> f c
 
-instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+         Arbitrary (Three a b c) where
     arbitrary = do
         a <- arbitrary
         b <- arbitrary
         c <- arbitrary
         return $ Three a b c
 
-instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+instance (Eq a, Eq b, Eq c) =>
+         EqProp (Three a b c) where
     (=-=) = eq
 
 threeTrigger :: Three (Int, Int, [Int]) (Int, Int, [Int]) (Int, Int, [Int])
@@ -176,12 +178,11 @@ threeTrigger = undefined
 testThreeTraversable :: IO ()
 testThreeTraversable = quickBatch $ traversable threeTrigger
 
-
-
 -- Three'
-
 data Three' a b =
-    Three' a b b
+    Three' a
+           b
+           b
     deriving (Eq, Show)
 
 instance Functor (Three' a) where
@@ -193,14 +194,16 @@ instance Foldable (Three' a) where
 instance Traversable (Three' a) where
     traverse f (Three' a b b') = Three' a <$> f b <*> f b'
 
-instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+instance (Arbitrary a, Arbitrary b) =>
+         Arbitrary (Three' a b) where
     arbitrary = do
-        a  <- arbitrary
-        b  <- arbitrary
+        a <- arbitrary
+        b <- arbitrary
         b' <- arbitrary
         return $ Three' a b b'
 
-instance (Eq a, Eq b) => EqProp (Three' a b) where
+instance (Eq a, Eq b) =>
+         EqProp (Three' a b) where
     (=-=) = eq
 
 three'Trigger :: Three' (Int, Int, [Int]) (Int, Int, [Int])
@@ -209,7 +212,37 @@ three'Trigger = undefined
 testThree'Traversable :: IO ()
 testThree'Traversable = quickBatch $ traversable three'Trigger
 
+-- S
+data S n a =
+    S (n a)
+      a
+    deriving (Eq, Show)
 
+instance Functor n =>
+         Functor (S n) where
+    fmap f (S x y) = S (fmap f x) (f y)
+
+instance Foldable n =>
+         Foldable (S n) where
+    foldMap f (S n a) = foldMap f n <> f a
+
+instance Traversable n =>
+         Traversable (S n) where
+    traverse g (S n a) = S <$> traverse g n <*> g a
+
+instance (Arbitrary (n a), CoArbitrary (n a), Arbitrary a, CoArbitrary a) =>
+         Arbitrary (S n a) where
+    arbitrary = S <$> arbitrary <*> arbitrary
+
+instance (Eq (n a), Eq a) =>
+         EqProp (S n a) where
+    (=-=) = eq
+
+sTrigger :: S Maybe (Int, Int, [Int])
+sTrigger = undefined
+
+testSTraversable :: IO ()
+testSTraversable = quickBatch $ traversable sTrigger
 
 testAll :: IO ()
 testAll = do
@@ -219,3 +252,4 @@ testAll = do
     testListTraversable
     testThreeTraversable
     testThree'Traversable
+    testSTraversable
