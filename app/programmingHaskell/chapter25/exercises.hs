6258d346f97cbc09c9be4538331c7fd2461e3e06
Martin Rist <mlr-github@martinrist.com>
2017-07-12 22:56:28 +0100

    Exercises - Foldable and Traversable instances for Compose

--- app/programmingHaskell/chapter25/exercises.hs
+++ app/programmingHaskell/chapter25/exercises.hs
@@ -1,6 +1,43 @@
+{-# LANGUAGE InstanceSigs #-}
+
+import Data.Monoid
+
 ----------------------------
 -- Chapter 25 - Exercises --
 ----------------------------
 
 main :: IO ()
 main = undefined
+
+
+---------------------------------------
+-- 25.6 Exercises: Compose Instances --
+---------------------------------------
+
+newtype Compose f g a =
+    Compose { getCompose :: f (g a) }
+    deriving (Eq, Show)
+
+
+
+-- Foldable
+-----------
+
+instance (Foldable f, Foldable g) =>
+          Foldable (Compose f g) where
+    foldMap :: Monoid m => (a -> m) -> Compose f g a -> m
+    foldMap fam (Compose fga) = (foldMap . foldMap) fam fga
+
+
+
+-- Traversable
+--------------
+instance (Functor f, Functor g) =>
+          Functor (Compose f g) where
+    fmap f (Compose fga) =
+        Compose $ (fmap . fmap) f fga
+
+instance (Traversable f, Traversable g) =>
+          Traversable (Compose f g) where
+    traverse :: Applicative p => (a -> p b) -> Compose f g a -> p (Compose f g b)
+    traverse fapb (Compose fga) = Compose <$> (traverse . traverse) fapb fga
