c52d5d6aa77fac6c748d9cac51dd424e2f4ed226
Martin Rist <mlr-github@martinrist.com>
2017-07-13 08:32:30 +0100

    Complete chapter 25 exercises

--- app/programmingHaskell/chapter25/exercises.hs
+++ app/programmingHaskell/chapter25/exercises.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
 
-import Data.Monoid
+import           Data.Monoid
 
 ----------------------------
 -- Chapter 25 - Exercises --
@@ -41,3 +41,73 @@ instance (Traversable f, Traversable g) =>
           Traversable (Compose f g) where
     traverse :: Applicative p => (a -> p b) -> Compose f g a -> p (Compose f g b)
     traverse fapb (Compose fga) = Compose <$> (traverse . traverse) fapb fga
+
+
+-- Bifunctor instances
+----------------------
+
+class Bifunctor p where
+    {-# MINIMAL bimap | first, second #-}
+
+    bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+    bimap f g = first f . second g
+
+    first :: (a -> b)  -> p a c -> p b c
+    first f = bimap f id
+
+    second :: (b -> c) -> p a b -> p a c
+    second = bimap id
+
+
+-- 1 - Deux
+data Deux a b = Deux a b
+    deriving (Eq, Show)
+
+instance Bifunctor Deux where
+    bimap f g (Deux a b) = Deux (f a) (g b)
+
+
+-- 2 - Const
+data Const a b = Const a
+    deriving (Eq, Show)
+
+instance Bifunctor Const where
+    bimap f g (Const a) = Const $ f a
+
+
+-- 3 - Drei
+data Drei a b c = Drei a b c
+    deriving (Eq, Show)
+
+instance Bifunctor (Drei a) where
+    bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+
+-- 4 - SuperDrei
+data SuperDrei a b c = SuperDrei a b
+    deriving (Eq, Show)
+
+instance Bifunctor (SuperDrei a) where
+    bimap f g (SuperDrei a b) = SuperDrei a (f b)
+
+
+-- 5 - SemiDrei
+data SemiDrei a b c = SemiDrei a
+    deriving (Eq, Show)
+
+instance Bifunctor (SemiDrei a) where
+    bimap f g (SemiDrei a) = SemiDrei a
+
+
+-- 6 - Quadriceps
+data Quadriceps a b c d = Quadzz a b c d
+    deriving (Eq, Show)
+
+instance Bifunctor (Quadriceps a b) where
+    bimap f g (Quadzz a b c d) = Quadzz a b (f c) (g d)
+
+
+-- 7 - Either
+instance Bifunctor Either where
+    bimap f g (Left a)  = Left (f a)
+    bimap f g (Right b) = Right (g b)
