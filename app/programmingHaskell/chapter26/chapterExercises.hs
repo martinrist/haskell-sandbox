3d50e3fb389c9fa64cc785879d01f0c2928efe64
Martin Rist <mlr-github@martinrist.com>
2017-07-18 17:04:37 +0100

    Complete 'Fix the code' exercise

--- app/programmingHaskell/chapter26/chapterExercises.hs
+++ app/programmingHaskell/chapter26/chapterExercises.hs
@@ -9,68 +9,67 @@ import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State
 import           Data.Functor.Identity
 
-
 ------------------------------------
 -- Chapter 26 - Chapter Exercises --
 ------------------------------------
 main :: IO ()
 main = undefined
 
-
 --------------------
 -- Write the code --
 --------------------
-
 -- Exercise 1 - rDec
-
-rDec :: Num a => Reader a a
+rDec
+    :: Num a
+    => Reader a a
 rDec = ReaderT (\a -> Identity (a - 1))
 
-
 -- Exercise 2 - rDec - pointfree version
-rDec' :: Num a => Reader a a
+rDec'
+    :: Num a
+    => Reader a a
 rDec' = ReaderT (Identity . subtract 1)
 
-
 -- Exercise 3 / 4 - rShow
-
-rShow :: Show a => ReaderT a Identity String
+rShow
+    :: Show a
+    => ReaderT a Identity String
 rShow = ReaderT (Identity . show)
 
-
 -- Exercise 5 - rPrintAndInc
-
-rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
-rPrintAndInc = ReaderT $ \a -> do
-    putStrLn $ "Hi: " ++ show a
-    return (a + 1)
-
+rPrintAndInc
+    :: (Num a, Show a)
+    => ReaderT a IO a
+rPrintAndInc =
+    ReaderT $ \a -> do
+        putStrLn $ "Hi: " ++ show a
+        return (a + 1)
 
 -- Exercise 6 - sPrintIncAccum
-sPrintIncAccum :: (Num a, Show a) => StateT a IO String
-sPrintIncAccum = StateT $ \a -> do
-    putStrLn $ "Hi: " ++ show a
-    return (show a, a + 1)
-
-
+sPrintIncAccum
+    :: (Num a, Show a)
+    => StateT a IO String
+sPrintIncAccum =
+    StateT $ \a -> do
+        putStrLn $ "Hi: " ++ show a
+        return (show a, a + 1)
 
 ------------------
 -- Fix the code --
 -------------------
-
 isValid :: String -> Bool
 isValid v = '!' `elem` v
 
 maybeExcite :: MaybeT IO String
-maybeExcite = MaybeT $ do
-    v <- getLine
-    pure (guard $ isValid v) :: IO (Maybe ())
-    return (pure v)
+maybeExcite =
+    MaybeT $ do
+        v <- getLine
+        return $ guard (isValid v) >> pure v
 
 doExcite :: IO ()
 doExcite = do
     putStrLn "say something excite!"
     excite <- runMaybeT maybeExcite
     case excite of
-         Nothing -> putStrLn "MOAR EXCITE"
-         Just e  -> putStrLn ("Good, was very excite: " ++ e)
+        Nothing -> putStrLn "MOAR EXCITE"
+        Just e  -> putStrLn ("Good, was very excite: " ++ e)
