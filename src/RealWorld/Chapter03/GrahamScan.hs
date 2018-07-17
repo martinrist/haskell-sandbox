59d041454c3c09614d99c9edd1e101233ca18b42
Martin Rist <mlr-github@martinrist.com>
2018-07-17 09:45:41 +0100

    Add guard to remove incomplete match warning

--- src/RealWorld/Chapter03/GrahamScan.hs
+++ src/RealWorld/Chapter03/GrahamScan.hs
@@ -33,6 +33,7 @@ turnDirection (Point x1 y1, Point x2 y2, Point x3 y3)
     | crossZ == 0 = Straight
     | crossZ > 0  = CounterClockwise
     | crossZ < 0  = Clockwise
+    | otherwise   = Straight
     where crossZ = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
 
 
