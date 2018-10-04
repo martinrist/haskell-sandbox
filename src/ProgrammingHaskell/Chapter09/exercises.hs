4f2d9b1648efb3fbe72d7d972cc9e253133e66f1
Martin Rist <mlr-github@martinrist.com>
2018-10-04 11:05:28 +0100

    Finish EnumFromTo exercise

--- src/ProgrammingHaskell/Chapter09/exercises.hs
+++ src/ProgrammingHaskell/Chapter09/exercises.hs
@@ -3,71 +3,48 @@
 ------------------------
 import Data.Char
 
--- Exercise : EnumFromTo
-
-eftBool :: Bool -> Bool -> [Bool]
-eftBool f t
-    | f == t        = [f]
-    | f > t         = []
-    | otherwise     = f:[] ++ (eftBool (succ f) t)
-
-eftOrd :: Ordering -> Ordering -> [Ordering]
-eftOrd f t
-    | f == t        = [f]
-    | f > t         = []
-    | otherwise     = f:[] ++ (eftOrd (succ f) t)
-
-eftInt :: Int -> Int -> [Int]
-eftInt f t
-    | f == t        = [f]
-    | f > t         = []
-    | otherwise     = f:[] ++ (eftInt (succ f) t)
-
-eftChar :: Char -> Char -> [Char]
-eftChar f t
-    | f == t        = [f]
-    | f > t         = []
-    | otherwise     = f:[] ++ (eftChar (succ f) t)
-
-
-
 -- Exercises : Thy Fearful Symmetry
 -----------------------------------
-
 -- Question 1
 myWords :: String -> [String]
 myWords s
-         | s == ""       = []
-         | otherwise     = (takeWhile notBlank s) : [] ++ (myWords (dropWhile blank $ dropWhile notBlank s))
-         where notBlank  = (/= ' ')
-               blank     = (== ' ')
+    | s == "" = []
+    | otherwise =
+        (takeWhile notBlank s) :
+        [] ++ (myWords (dropWhile blank $ dropWhile notBlank s))
+  where
+    notBlank = (/= ' ')
+    blank = (== ' ')
 
 -- Question 2
-
 firstSen = "Tyger Tyger, burning bright\n"
-secondSen = "In the forests of the night\n"
-thirdSen = "What immortal hand or eye\n"
-fourthSen = "Could frame thy fearful symmetry?"
-sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
 
+secondSen = "In the forests of the night\n"
+
+thirdSen = "What immortal hand or eye\n"
+
+fourthSen = "Could frame thy fearful symmetry?"
+
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
 
 tokenise :: String -> Char -> [String]
 tokenise s c
-         | s == ""       = []
-         | otherwise     = (takeWhile notMatch s) : [] ++ (tokenise (dropWhile match $ dropWhile notMatch s) c)
-         where notMatch  = (/= c)
-               match     = (== c)
+    | s == "" = []
+    | otherwise =
+        (takeWhile notMatch s) :
+        [] ++ (tokenise (dropWhile match $ dropWhile notMatch s) c)
+  where
+    notMatch = (/= c)
+    match = (== c)
 
 myLines :: String -> [String]
 myLines s = tokenise s '\n'
 
-
-
 -- Exercises: Square Cube
 -------------------------
+mySqr = [x ^ 2 | x <- [1 .. 5]]
 
-mySqr = [ x^2 | x <- [1..5]]
-myCube = [ x^3 | x <- [1..5]]
+myCube = [x ^ 3 | x <- [1 .. 5]]
 
 mySquaresAndCubes = [(x, y) | x <- mySqr, y <- myCube]
 
@@ -75,10 +52,8 @@ mySquaresAndCubesUnder50 = [(x, y) | x <- mySqr, x < 50, y <- myCube, y < 50]
 
 countOfMySquaresAndCubesUnder50 = length mySquaresAndCubesUnder50
 
-
 -- Exercises: Filtering
 -----------------------
-
 -- Question 1
 multiplesOf3 = filter (\x -> (x `mod` 3) == 0)
 
@@ -89,50 +64,43 @@ howManyMultiples = length . multiplesOf3
 myFilter :: String -> [String]
 myFilter = (filter (\w -> not (elem w ["the", "a", "an"]))) . words
 
-
 -- Exercises: Zipping
 ---------------------
-
 -- Question 1
-zip' :: [a] -> [b] -> [(a,b)]
-zip' _ []          = []
-zip' [] _          = []
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
 zip' (x:xs) (y:ys) = (x, y) : (zip' xs ys)
 
 -- Question 2
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ _ []          = []
-zipWith' _ [] _          = []
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
 zipWith' f (x:xs) (y:ys) = f x y : (zipWith' f xs ys)
 
 -- Question 3
 zip'' :: [a] -> [b] -> [(a, b)]
 zip'' = zipWith' (\x y -> (x, y))
 
-
 -- Chapter Exercises
 --------------------
-
 -- Question 2
 filterUpper = filter isUpper
 
 -- Question 3
 capitaliseFirst :: String -> String
-capitaliseFirst []     = []
+capitaliseFirst [] = []
 capitaliseFirst (x:xs) = (toUpper x) : xs
 
 -- Question 4
 capitalise :: String -> String
-capitalise []     = []
+capitalise [] = []
 capitalise (x:xs) = (toUpper x) : (capitalise xs)
 
 -- Questions 5 & 6
 getFirstAsCapital = toUpper . head
 
-
-
 -- Standard functions
-
 myAnd :: [Bool] -> Bool
 myAnd [] = True
 myAnd (x:xs) = x && myAnd xs
@@ -151,7 +119,7 @@ myAny pred (x:xs) = pred x || (myAny pred xs)
 myElem :: Eq a => a -> [a] -> Bool
 myElem _ [] = False
 myElem x (y:ys)
-    | x == y    = True
+    | x == y = True
     | otherwise = myElem x ys
 
 myElem' :: Eq a => a -> [a] -> Bool
@@ -181,14 +149,15 @@ myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
 myMaximumBy _ [x] = x
 myMaximumBy f (x:xs)
     | f x maxRest == GT = x
-    | otherwise         = maxRest
-    where maxRest = myMaximumBy f xs
+    | otherwise = maxRest
+  where
+    maxRest = myMaximumBy f xs
 
 -- Question 9
 myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
 myMinimumBy _ [x] = x
 myMinimumBy f (x:xs)
     | f x minRest == LT = x
-    | otherwise         = minRest
-    where minRest = myMinimumBy f xs
-
+    | otherwise = minRest
+  where
+    minRest = myMinimumBy f xs
