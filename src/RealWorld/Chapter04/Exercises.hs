5fbda9b6462dfd09990d7bc720a4a01b3a5366af
Martin Rist <mlr-github@martinrist.com>
2018-10-05 15:30:51 +0100

    Clean up unimplemented tests

--- src/RealWorld/Chapter04/Exercises.hs
+++ src/RealWorld/Chapter04/Exercises.hs
@@ -1,7 +1,6 @@
 module RealWorld.Chapter04.Exercises where
 
 -- 1. Safe definitions of standard partial list functions from Prelude
-
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead xs = Just $ head xs
@@ -18,13 +17,12 @@ safeInit :: [a] -> Maybe [a]
 safeInit [] = Nothing
 safeInit xs = Just $ init xs
 
-
 -- 2. splitWith function to split an input list on every element for 
 -- which the predicate returns `False`
-
 splitWith :: (a -> Bool) -> [a] -> [[a]]
 splitWith _ [] = []
-splitWith p xs = let start     = takeWhile p xs
-                     end       = dropWhile p xs
-                     rest      = dropWhile (not . p) end
-                 in start : splitWith p rest
+splitWith p xs =
+    let start = takeWhile p xs
+        end = dropWhile p xs
+        rest = dropWhile (not . p) end
+     in start : splitWith p rest
--- test/RealWorld/Chapter04/ExercisesSpec.hs
+++ test/RealWorld/Chapter04/ExercisesSpec.hs
@@ -1,68 +1,55 @@
 module RealWorld.Chapter04.ExercisesSpec where
 
-import Test.Hspec
 import RealWorld.Chapter04.Exercises
+import Test.Hspec
 
 testExercise1 :: Spec
-testExercise1 = context "Exercise 1 Tests" $ do
-    testSafeHead
-    testSafeTail
-    testSafeLast
-    testSafeInit
+testExercise1 =
+    context "Exercise 1 Tests" $ do
+        testSafeHead
+        testSafeTail
+        testSafeLast
+        testSafeInit
 
 testSafeHead :: Spec
-testSafeHead = context "safeHead" $ do
-    it "works with empty list" $
-        safeHead ([] :: [()]) `shouldBe` Nothing
-    it "works with single element list" $
-        safeHead ['a'] `shouldBe` Just 'a'
-    it "works with two element list" $
-        safeHead "hi" `shouldBe` Just 'h'
+testSafeHead =
+    context "safeHead" $ do
+        it "works with empty list" $ safeHead ([] :: [()]) `shouldBe` Nothing
+        it "works with single element list" $ safeHead ['a'] `shouldBe` Just 'a'
+        it "works with two element list" $ safeHead "hi" `shouldBe` Just 'h'
 
 testSafeTail :: Spec
-testSafeTail = context "safeTail" $ do
-    it "works with empty list" $
-        safeTail ([] :: [()]) `shouldBe` Nothing
-    it "works with single element list" $
-        safeTail "a" `shouldBe` Just ""
-    it "works with two element list" $
-        safeTail "hi" `shouldBe` Just "i"
-    it "works with three element list" $
-        safeTail "foo" `shouldBe` Just "oo"
+testSafeTail =
+    context "safeTail" $ do
+        it "works with empty list" $ safeTail ([] :: [()]) `shouldBe` Nothing
+        it "works with single element list" $ safeTail "a" `shouldBe` Just ""
+        it "works with two element list" $ safeTail "hi" `shouldBe` Just "i"
+        it "works with three element list" $ safeTail "foo" `shouldBe` Just "oo"
 
 testSafeLast :: Spec
-testSafeLast = context "safeLast" $ do
-    it "works with empty list" $
-        safeLast ([] :: [()]) `shouldBe` Nothing
-    it "works with single element list" $
-        safeLast "a" `shouldBe` Just 'a'
-    it "works with two element list" $
-        safeLast "hi" `shouldBe` Just 'i'
-    it "works with three element list" $
-        safeLast "foo" `shouldBe` Just 'o'
+testSafeLast =
+    context "safeLast" $ do
+        it "works with empty list" $ safeLast ([] :: [()]) `shouldBe` Nothing
+        it "works with single element list" $ safeLast "a" `shouldBe` Just 'a'
+        it "works with two element list" $ safeLast "hi" `shouldBe` Just 'i'
+        it "works with three element list" $ safeLast "foo" `shouldBe` Just 'o'
 
 testSafeInit :: Spec
-testSafeInit = context "safeInit" $ do
-    it "works with empty list" $
-        safeInit ([] :: [()]) `shouldBe` Nothing
-    it "works with single element list" $
-        safeInit "a" `shouldBe` Just ""
-    it "works with two element list" $
-        safeInit "hi" `shouldBe` Just "h"
-    it "works with three element list" $
-        safeInit "foo" `shouldBe` Just "fo"
-
+testSafeInit =
+    context "safeInit" $ do
+        it "works with empty list" $ safeInit ([] :: [()]) `shouldBe` Nothing
+        it "works with single element list" $ safeInit "a" `shouldBe` Just ""
+        it "works with two element list" $ safeInit "hi" `shouldBe` Just "h"
+        it "works with three element list" $ safeInit "foo" `shouldBe` Just "fo"
 
 testExercise2 :: Spec
-testExercise2 = context "Exercise 2 Tests" $
-    testSplitWith
+testExercise2 = context "Exercise 2 Tests" $ testSplitWith
 
 testSplitWith :: Spec
-testSplitWith = context "splitWith" $ do
+testSplitWith =
+    context "splitWith" $
     it "works" $
-        pendingWith "Not implemented yet"
-
-
+    splitWith (\x -> x `mod` 3 == 0) [1 .. 10] `shouldBe` [[], [3], [6], [9]]
 
 spec :: Spec
 spec = do
