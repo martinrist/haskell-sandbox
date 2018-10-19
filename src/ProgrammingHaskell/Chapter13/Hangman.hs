2ac255a2a7564fc396e62bcbe15f6d5ba9d16c71
Martin Rist <mlr-github@martinrist.com>
2018-10-19 09:22:35 +0100

    Move more from lib to app

--- src/ProgrammingHaskell/Chapter13/Hangman.hs
+++ src/ProgrammingHaskell/Chapter13/Hangman.hs
@@ -1,12 +1,8 @@
 module ProgrammingHaskell.Chapter13.Hangman where
 
-import           Control.Monad                  ( forever )
 import           Data.Char                      ( toLower )
 import           Data.List                      ( intersperse )
-import           Data.Maybe                     ( isJust
-                                                , fromMaybe
-                                                )
-import           System.Exit                    ( exitSuccess )
+import           Data.Maybe                     ( fromMaybe )
 import           System.Random                  ( randomRIO )
 
 newtype WordList =
@@ -81,29 +77,3 @@ handleGuess puzzle guess = do
         (False, _) -> do
             putStrLn "This character wasn't in the word, try again."
             return (fillInCharacter puzzle guess)
-
-gameOver :: Puzzle -> IO ()
-gameOver (Puzzle wordToGuess _ guessed) = if length guessed > 7
-    then do
-        putStrLn "You lose!"
-        putStrLn $ "The word was: " ++ wordToGuess
-        exitSuccess
-    else return ()
-
-gameWin :: Puzzle -> IO ()
-gameWin (Puzzle _ filledInSoFar _) = if all isJust filledInSoFar
-    then do
-        putStrLn "You win!"
-        exitSuccess
-    else return ()
-
-runGame :: Puzzle -> IO ()
-runGame puzzle = forever $ do
-    gameOver puzzle
-    gameWin puzzle
-    putStrLn $ "Current puzzle is: " ++ show puzzle
-    putStr "Guess a letter: "
-    guess <- getLine
-    case guess of
-        [c] -> handleGuess puzzle c >>= runGame
-        _   -> putStrLn "Your guess must be a single character"
