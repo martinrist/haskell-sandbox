3c7b75ee583c3a7594f6bf249c09be8a709975a9
Martin Rist <mlr-github@martinrist.com>
2018-11-19 15:41:52 +0000

    Complete Semigroup exercises

--- src/ProgrammingHaskell/Chapter15/Exercises/Semigroup.hs
+++ src/ProgrammingHaskell/Chapter15/Exercises/Semigroup.hs
@@ -1,7 +1,14 @@
 module ProgrammingHaskell.Chapter15.Exercises.Semigroup where
 
-import           Test.QuickCheck
+import           Test.QuickCheck         hiding ( Success
+                                                , Failure
+                                                )
 import           Control.Applicative
+
+-- This gives us an instance of `Show` for functions
+-- which in turn allows us to derive an instance for `Combine`
+import           Text.Show.Functions
+
 
 -- Question 1 - Trivial
 
@@ -12,6 +19,7 @@ instance Arbitrary Trivial where
 
 instance Semigroup Trivial where
     _ <> _ = Trivial
+
 
 -- Question 2 - Identity
 
@@ -24,6 +32,7 @@ instance Arbitrary a => Arbitrary (Identity a) where
 instance Semigroup (Identity a) where
     Identity a <> Identity b = Identity a
 
+
 -- Question 3  - Two
 
 data Two a b = Two a b
@@ -35,7 +44,9 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
 instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
     (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
 
+
 -- Question 4 - Three
+
 data Three a b c = Three a b c
     deriving (Eq, Show)
 
@@ -45,7 +56,9 @@ instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) wher
 instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
     (Three a b c) <> (Three a' b' c') = Three (a <> a') (b <> b') (c <> c')
 
+
 -- Question 5 - Four
+
 data Four a b c d = Four a b c d
     deriving (Eq, Show)
 
@@ -54,6 +67,7 @@ instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four
 
 instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
     (Four a b c d) <> (Four a' b' c' d') = Four (a <> a') (b <> b') (c <> c') (d <> d')
+
 
 -- Question 6 - BoolConj
 
@@ -65,6 +79,7 @@ instance Arbitrary BoolConj where
 instance Semigroup BoolConj where
     (BoolConj a) <> (BoolConj b) = BoolConj (a && b)
 
+
 -- Question 7 - BoolDisj
 
 newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
@@ -74,3 +89,59 @@ instance Arbitrary BoolDisj where
 
 instance Semigroup BoolDisj where
     (BoolDisj a) <> (BoolDisj b) = BoolDisj (a || b)
+
+
+-- Question 8 - Or
+
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Fst a, Snd b]
+
+instance Semigroup (Or a b) where
+    Snd a <> _     = Snd a
+    _     <> Snd b = Snd b
+    Fst a <> Fst b = Fst b
+
+
+-- Question 9 - Combine
+
+newtype Combine a b = Combine { unCombine :: a -> b } deriving Show
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+    arbitrary = Combine <$> arbitrary
+
+instance Semigroup b => Semigroup (Combine a b) where
+    (Combine f) <> (Combine g) = Combine $ \a -> f a <> g a
+
+
+-- Question 10 - Comp
+
+newtype Comp a = Comp { unComp :: a -> a } deriving Show
+
+instance (CoArbitrary a, Arbitrary a) => Arbitrary (Comp a) where
+    arbitrary = Comp <$> arbitrary
+
+-- Notice that this instance doesn't require `Semigroup a`.  Since the function
+-- inside `Comp` is of type `a -> a`, we can just use normal function composition
+instance Semigroup (Comp a) where
+    (Comp f) <> (Comp g) = Comp $ \a -> f (g a)
+
+
+-- Question 11 - Validation
+
+data Validation a b = Failure a | Success b deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Failure a, Success b]
+
+instance Semigroup a => Semigroup (Validation a b) where
+    Success a <> _     = Success a
+    _     <> Success b = Success b
+    Failure a <> Failure b = Failure $ a <> b
--- test/ProgrammingHaskell/Chapter15/Exercises/SemigroupSpec.hs
+++ test/ProgrammingHaskell/Chapter15/Exercises/SemigroupSpec.hs
@@ -1,21 +1,31 @@
-{-# LANGUAGE TypeOperators #-}
-
 module ProgrammingHaskell.Chapter15.Exercises.SemigroupSpec where
 
 import           ProgrammingHaskell.Chapter15.Exercises.Semigroup
 import           ProgrammingHaskell.Chapter15.SemigroupLaws
 import           Test.Hspec
-import           Test.QuickCheck
+import           Test.QuickCheck         hiding ( Success
+                                                , Failure
+                                                )
+import           Data.Semigroup
+
+
+-- Question 1 - Trivial
 
 testTrivial :: Spec
 testTrivial =
     context "Trivial" $ it "Obeys Semigroup associative law" $ property
         (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Bool)
 
+
+-- Question 2 - Identity
+
 testIdentity :: Spec
 testIdentity =
     context "Identity" $ it "Obeys Semigroup associative law" $ property
         (semigroupAssoc :: Identity Int -> Identity Int -> Identity Int -> Bool)
+
+
+-- Question 3 - Two
 
 testTwo :: Spec
 testTwo = context "Two" $ it "Obeys Semigroup associative law" $ property
@@ -25,6 +35,9 @@ testTwo = context "Two" $ it "Obeys Semigroup associative law" $ property
       -> Bool
     )
 
+
+-- Question 4 - Three
+
 testThree :: Spec
 testThree = context "Three" $ it "Obeys Semigroup associative law" $ property
     (semigroupAssoc :: Three String String String
@@ -32,6 +45,9 @@ testThree = context "Three" $ it "Obeys Semigroup associative law" $ property
       -> Three String String String
       -> Bool
     )
+
+
+-- Question 5 - Four
 
 testFour :: Spec
 testFour = context "Four" $ it "Obeys Semigroup associative law" $ property
@@ -41,15 +57,144 @@ testFour = context "Four" $ it "Obeys Semigroup associative law" $ property
       -> Bool
     )
 
+
+-- Question 6 - BoolConj
+
 testBoolConj :: Spec
-testBoolConj = context "BoolConj" $
-    it "Obeys Semigroup associative law" $
-        property (semigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
+testBoolConj = context "BoolConj" $ do
+    it "Obeys Semigroup associative law" $ property
+        (semigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
+    it "Behaves like conjunction for two `True`s"
+        $          BoolConj True
+        <>         BoolConj True
+        `shouldBe` BoolConj True
+    it "Behaves like conjunction for `True` and `False`"
+        $          BoolConj True
+        <>         BoolConj False
+        `shouldBe` BoolConj False
+
+
+-- Question 7 - BoolDisj
 
 testBoolDisj :: Spec
-testBoolDisj = context "BoolDisj" $
-    it "Obeys Semigroup associative law" $
-        property (semigroupAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Bool)
+testBoolDisj = context "BoolDisj" $ do
+    it "Obeys Semigroup associative law" $ property
+        (semigroupAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Bool)
+    it "Behaves like disjunction for two `True`s"
+        $          BoolDisj True
+        <>         BoolDisj True
+        `shouldBe` BoolDisj True
+    it "Behaves like disjunction for `True` and `False`"
+        $          BoolDisj True
+        <>         BoolDisj False
+        `shouldBe` BoolDisj True
+
+
+-- Question 8 - Or
+
+testOr :: Spec
+testOr = context "Or" $ do
+    it "Obeys Semigroup associative law" $ property
+        (semigroupAssoc :: Or Int String
+          -> Or Int String
+          -> Or Int String
+          -> Bool
+        )
+    it "Discards `Fst` when followed by `Snd`" $ Fst 1 <> Snd 2 `shouldBe` Snd 2
+    it "Discards `Fst` when followed by `Fst`"
+        $          Fst 1
+        <>         Fst 2
+        `shouldBe` (Fst 2 :: Or Int Int)
+    it "Keeps `Snd` when followed by `Fst`" $ Snd 1 <> Fst 2 `shouldBe` Snd 1
+    it "Keeps `Snd` when followed by `Snd`"
+        $          Snd 1
+        <>         Snd 2
+        `shouldBe` (Snd 1 :: Or Int Int)
+
+
+-- Question 9 - Combine
+
+f :: Combine Int (Sum Int)
+f = Combine $ \n -> Sum (n + 1)
+
+g :: Combine Int (Sum Int)
+g = Combine $ \n -> Sum (n - 1)
+
+combineAssoc
+    :: (Eq b, Semigroup b)
+    => a
+    -> Combine a b
+    -> Combine a b
+    -> Combine a b
+    -> Bool
+combineAssoc v a b c =
+    unCombine (a <> (b <> c)) v == unCombine ((a <> b) <> c) v
+
+testCombine :: Spec
+testCombine = context "Combine" $ do
+    it "Obeys Semigroup associative law" $ property
+        (combineAssoc :: Int
+          -> Combine Int String
+          -> Combine Int String
+          -> Combine Int String
+          -> Bool
+        )
+    it "`unCombine (f <> g) 0` returns 0" $ unCombine (f <> g) 0 `shouldBe` Sum
+        0
+    it "`unCombine (f <> g) 1` returns 2" $ unCombine (f <> g) 1 `shouldBe` Sum
+        2
+    it "`unCombine (f <> f) 1` returns 4" $ unCombine (f <> f) 1 `shouldBe` Sum
+        4
+    it "`unCombine (g <> f) 1` returns 2" $ unCombine (g <> f) 1 `shouldBe` Sum
+        2
+
+
+-- Question 10 - Comp
+
+inc :: Comp Int
+inc = Comp $ \n -> n + 1
+
+double :: Comp Int
+double = Comp $ \n -> n * 2
+
+compAssoc :: Eq a => a -> Comp a -> Comp a -> Comp a -> Bool
+compAssoc v a b c = unComp (a <> (b <> c)) v == unComp ((a <> b) <> c) v
+
+testComp :: Spec
+testComp = context "Comp" $ do
+    it "Obeys Semigroup associative law" $ property
+        (compAssoc :: Int -> Comp Int -> Comp Int -> Comp Int -> Bool)
+    it "`unComp (double <> inc) 3` should return 8`"
+        $          unComp (double <> inc) 3
+        `shouldBe` 8
+
+
+-- Question 11 - Valdiation
+
+testValidation :: Spec
+testValidation = context "Validation" $ do
+    it "Obeys Semigroup associative law" $ property
+        (semigroupAssoc :: Validation String String
+          -> Validation String String
+          -> Validation String String
+          -> Bool
+        )
+    it "Retains initial `Success` followed by `Failure`"
+        $          Success 1
+        <>         Failure "blah"
+        `shouldBe` Success 1
+    it "Mappends multiple `Failure`s"
+        $          Failure "woot"
+        <>         Failure "blah"
+        `shouldBe` (Failure "wootblah" :: Validation String String)
+    it "Retains first `Success` followed by another `Success`"
+        $          Success 1
+        <>         Success 2
+        `shouldBe` (Success 1 :: Validation String Int)
+    it "Skips first `Failure` followed by `Success`"
+        $          Failure "woot"
+        <>         Success 2
+        `shouldBe` Success 2
 
 spec :: Spec
 spec = do
@@ -60,3 +205,7 @@ spec = do
     testFour
     testBoolConj
     testBoolDisj
+    testOr
+    testCombine
+    testComp
+    testValidation
