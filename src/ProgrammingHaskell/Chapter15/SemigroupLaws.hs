e7eae89895e5d14c52453c541e28f37ca17c4bb0
Martin Rist <mlr-github@martinrist.com>
2018-11-23 11:20:32 +0000

    Refactor to remove duplication

--- src/ProgrammingHaskell/Chapter15/SemigroupLaws.hs
+++ src/ProgrammingHaskell/Chapter15/SemigroupLaws.hs
@@ -1,7 +1,7 @@
 module ProgrammingHaskell.Chapter15.SemigroupLaws where
 
 -- Associativity
-semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
-semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
-
 type SemigroupAssoc m = m -> m -> m -> Bool
+
+semigroupAssoc :: (Eq m, Semigroup m) => SemigroupAssoc m
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
