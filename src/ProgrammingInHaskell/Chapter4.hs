0582c6f2fe51aca6da4b2116e75014acb6a6ead5
Martin Rist <mlr@antaeus.local>
2019-10-27 13:48:32 +0000

    'Programming in Haskell' Chatper 4 exercises

---
+++ src/ProgrammingInHaskell/Chapter4.hs
@@ -0,0 +1,57 @@
+module ProgrammingInHaskell.Chapter4 where
+
+-- | Calculate the absolute value of n
+abs :: Integral a => a -> a
+abs n | n >= 0    = n
+      | otherwise = -n
+
+-- | Calculate the signum value of n
+signum :: Integral a => a -> a
+signum n | n < 0     = -1
+         | n == 0    = 0
+         | otherwise = 1
+
+-- Exercise 4.1 - `halve`
+
+-- | Split a list into two halves
+halve :: [a] -> ([a], [a])
+halve xs = (take n xs, drop n xs) where n = length xs `div` 2
+
+
+-- Exercise 4.2 - `third`
+
+-- | Return the third element in the list, using `head` and `tail`:
+thirdUsingHeadAndTail :: [a] -> a
+thirdUsingHeadAndTail = head . tail . tail
+
+-- | Return the third element in the list, using `!!`:
+thirdUsingListIndexing :: [a] -> a
+thirdUsingListIndexing xs = xs !! 2
+
+-- | Return the third element in the list, using pattern matching
+thirdUsingPatternMatching :: [a] -> a
+thirdUsingPatternMatching (_ : _ : x : xs) = x
+
+
+-- Exercise 4.3 - `safetail`
+
+-- | Return the tail of the list, or [] if empty, using conditionals
+safetailUsingConditionals :: [a] -> [a]
+safetailUsingConditionals xs = if null xs then [] else tail xs
+
+-- | Return the tail of the list, or [] if empty, using guards
+safetailUsingGuards :: [a] -> [a]
+safetailUsingGuards xs | null xs   = []
+                       | otherwise = tail xs
+
+-- | Return the tail of the list, or [] if empty, using pattern matching
+safetailUsingPatternMatching :: [a] -> [a]
+safetailUsingPatternMatching []       = []
+safetailUsingPatternMatching (x : xs) = xs
+
+
+-- Exercise 4.4 - Disjunction using pattern matching
+
+(||) :: Bool -> Bool -> Bool
+False || False = False
+_     || _     = True
---
+++ test/ProgrammingInHaskell/Chapter4Spec.hs
@@ -0,0 +1,58 @@
+module ProgrammingInHaskell.Chapter4Spec where
+
+import           ProgrammingInHaskell.Chapter4
+import           Test.Hspec
+
+testHalve :: SpecWith ()
+testHalve = 
+    describe "Exercise 4.1 - `halve`" $ do
+        it "Correctly halves an empty list" $
+            halve ([] :: [Int]) `shouldBe` ([], [])
+        it "Correctly halves a single element list" $
+            halve [1] `shouldBe` ([], [1])
+        it "Correctly halves a two element list" $
+            halve [1, 2] `shouldBe` ([1], [2])
+        it "Correctly halves a larger odd-numbered element list" $
+            halve [1..11] `shouldBe` ([1..5], [6..11])
+
+testThird :: SpecWith ()
+testThird =
+    describe "Exercise 4.2 - `third`" $ do
+        it "Works with implementation using `head` and `tail`" $
+            thirdUsingHeadAndTail [1, 2, 3] `shouldBe` 3
+        it "Works with implementation using list indexing" $
+            thirdUsingListIndexing [1, 2, 3] `shouldBe` 3
+        it "Works with implementation using pattern matching" $
+            thirdUsingPatternMatching [1, 2, 3] `shouldBe` 3
+
+testSafetail :: SpecWith ()
+testSafetail =
+    describe "Exercise 4.3 - `safetail`" $ do
+        it "Works with implementation using conditionals" $ do
+            safetailUsingConditionals ([] :: [Int]) `shouldBe` []
+            safetailUsingConditionals [1] `shouldBe` []
+            safetailUsingConditionals [1, 2] `shouldBe` [2]
+        it "Works with implementation using guards" $ do
+            safetailUsingGuards ([] :: [Int]) `shouldBe` []
+            safetailUsingGuards [1] `shouldBe` []
+            safetailUsingGuards [1, 2] `shouldBe` [2]
+        it "Works with implementation using pattern matching" $ do
+            safetailUsingPatternMatching ([] :: [Int]) `shouldBe` []
+            safetailUsingPatternMatching [1] `shouldBe` []
+            safetailUsingPatternMatching [1, 2] `shouldBe` [2]
+
+testDisjunction :: SpecWith ()
+testDisjunction =
+    describe "Exercise 4.4 - `||` using pattern matching" $
+        it "Works as expected" $ do
+            False ProgrammingInHaskell.Chapter4.|| False `shouldBe` False
+            False ProgrammingInHaskell.Chapter4.|| True `shouldBe` True
+            True ProgrammingInHaskell.Chapter4.|| False `shouldBe` True
+            True ProgrammingInHaskell.Chapter4.|| True `shouldBe` True
+            
+spec :: Spec
+spec = do
+    testHalve
+    testThird
+    testSafetail
+    testDisjunction
