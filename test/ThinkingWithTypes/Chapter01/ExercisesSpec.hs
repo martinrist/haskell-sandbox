667f2d3a6eef55b170e6e0b5d97122c38a0a529c
Martin Rist <mlr-github@martinrist.com>
2019-11-04 15:23:27 +0000

    Test isomorphism both ways round

--- test/ThinkingWithTypes/Chapter01/ExercisesSpec.hs
+++ test/ThinkingWithTypes/Chapter01/ExercisesSpec.hs
@@ -8,42 +8,56 @@ import           Test.QuickCheck.Checkers
 
 -- Exercise 1.4-i - `(a^b)^c = a^(b×c)`
 
-type ISD = (Int, String) -> Double
+-- a ~ Double
+-- b ~ Int
+-- c ~ String
+type TISD = (Int, String) -> Double
+type ISD  = Int -> String -> Double
 
 testExpOfExpIsomorphism :: Spec
 testExpOfExpIsomorphism =
-    context "Exercise 1.4-i - `(a^b)^c = a^(b×c)`" $
-        it "`to` and `from` define an isomorphism" $
-            (fromExpOfExp . toExpOfExp :: ISD -> ISD) =-= id
+    context "Exercise 1.4-i - `(a^b)^c = a^(b×c)`" $ do
+        it "`from . to == id`" $
+            (fromExpOfExp . toExpOfExp :: TISD -> TISD) =-= id
+        it "`to . from == id`" $
+            (toExpOfExp . fromExpOfExp :: ISD -> ISD) =-= id
 
 
 
 -- Exercise 1.4-ii - `a^b x a^c = a ^ (b + c)`
 
-type EISD = Either Int String -> Double
+-- a ~ Double
+-- b ~ Int
+-- c ~ String
+type EISD  = Either Int String -> Double
+type TIDSD = (Int -> Double, String -> Double)
 
 testProdOfExpIsomorphism :: Spec
 testProdOfExpIsomorphism =
-    context "Exercise 1.4-ii - `a^b x a^c = a ^ (b + c)`" $
-        it "`to` and `from` define an isomorphism" $
+    context "Exercise 1.4-ii - `a^b x a^c = a ^ (b + c)`" $ do
+        it "`from . to == id`" $
             (fromProdOfExp . toProdOfExp :: EISD -> EISD) =-= id
+        it "`to . from == id`" $
+            (toProdOfExp . fromProdOfExp :: TIDSD -> TIDSD) =-= id
 
 
 
 -- Exercise 1.4-iii - `(a x b) ^ c = a^c x b^c`
 
-prop_expOfProdIsomorphism :: (Eq a, Eq b) => (c -> a, c -> b) -> c -> Bool
-prop_expOfProdIsomorphism (ca, cb) c =
-    let (ca', cb') = (fromExpOfProd . toExpOfProd) (ca, cb)
-    in  (ca c, cb c) == (ca' c, cb' c)
-
-type ISID = (Int -> String, Int -> Double)
+-- a ~ Double
+-- b ~ Int
+-- c ~ String
+type TSDSI = (String -> Double, String -> Int)
+type STDI  = String -> (Double, Int)
 
 testExpOfProdIsomorphism :: Spec
 testExpOfProdIsomorphism =
-    context "Exercise 1.4-iii - `(a x b) ^ c = a^c x b^c`" $
-        it "`to` and `from` define an isomorphism" $
-            (fromExpOfProd . toExpOfProd :: ISID -> ISID) =-= id
+    context "Exercise 1.4-iii - `(a x b) ^ c = a^c x b^c`" $ do
+        it "`from . to == id`" $
+            (fromExpOfProd . toExpOfProd :: TSDSI -> TSDSI) =-= id
+        it "`to . from == id`" $
+            (toExpOfProd . fromExpOfProd :: STDI -> STDI) =-= id
+
 
 
 spec :: Spec
