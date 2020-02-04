5a677e705446537ce745f759fb560d3da7d86bb4
Martin Rist <mlr-github@martinrist.com>
2020-02-04 11:18:51 +0000

    Complete Chapter 6

--- src/ThinkingWithTypes/Chapter06/Examples.hs
+++ src/ThinkingWithTypes/Chapter06/Examples.hs
@@ -2,6 +2,8 @@
 
 module ThinkingWithTypes.Chapter06.Examples where
 
+import ThinkingWithTypes.Chapter06.Exercises
+
 -- applyToFiveBroken :: (a -> a) -> Int
 -- This won't compile, because we can't guarantee that `f` can take an `Int`
 -- It's equivalent to either of:
@@ -22,3 +24,34 @@ runCont :: (forall r. (a -> r) -> r) -> a
 runCont f =
   let callback = id
     in f callback
+
+
+
+-- The `Cont` monad in action.  These functions all 'perform async operations'
+-- then call their given callbacks when completed
+
+withVersionNumber :: (Double -> r) -> r
+withVersionNumber f = f 1.0
+
+withTimestamp :: (Int -> r) -> r
+withTimestamp f = f 1532083362
+
+withOS :: (String -> r) -> r
+withOS f = f "linux"
+
+
+-- This is the 'pyramid-of-doom' style of combining these three functions:
+
+releaseString :: String
+releaseString =
+  withVersionNumber $ \version ->
+    withTimestamp $ \date ->
+      withOS $ \os ->
+        os ++ "-" ++ show version ++ "-" ++ show date
+
+releaseStringCont :: String
+releaseStringCont = runCont $ unCont $ do
+  version <- Cont withVersionNumber
+  date <- Cont withTimestamp
+  os <- Cont withOS
+  pure $ os ++ "-" ++ show version ++ "-" ++ show date
